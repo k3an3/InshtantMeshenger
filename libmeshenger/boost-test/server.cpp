73f75d520f3ea718ff800b955d5b6c03a81ef0be
Keane O'Kelley <korockinout13@gmail.com>
2015-09-21 09:01:55 -0500

    Follow style guide

--- libmeshenger/boost-test/server.cpp
+++ libmeshenger/boost-test/server.cpp
@@ -6,26 +6,28 @@
 using boost::asio::ip::tcp;
 using namespace std;
 
-const int port = 1234;
+const int PORT = 1234;
 
-int main() {
+int
+main()
+{
     boost::asio::io_service io_service;
     tcp::acceptor acceptor(io_service);
-    tcp::endpoint endpoint(tcp::v4(), port);
+    tcp::endpoint endpoint(tcp::v4(), PORT);
     acceptor.open(endpoint.protocol());
     acceptor.set_option(tcp::acceptor::reuse_address(true));
     //acceptor.set_option(boost::asio::socket_base::broadcast(true));
     tcp::socket socket(io_service);
     acceptor.bind(endpoint);
     acceptor.listen();
-    int i = 0;
+
     while(1) {
         char recv[1024];
         acceptor.accept(socket);
         tcp::endpoint e = socket.remote_endpoint();
         socket.read_some(boost::asio::buffer(recv));
         cerr << recv << " Got connection from: " << e.address() << "\n";
-        boost::asio::write(socket, boost::asio::buffer("hotdog\n", 6));
+        boost::asio::write(socket, boost::asio::buffer("hotdog\n", 7));
         socket.close();
     }
 }
