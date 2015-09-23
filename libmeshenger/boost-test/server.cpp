b6a71eac001cf69a5183a951375fe47cf10d3c96
Keane O'Kelley <korockinout13@gmail.com>
2015-09-23 10:29:35 -0500

    Fix coding style- use tabs

--- libmeshenger/boost-test/server.cpp
+++ libmeshenger/boost-test/server.cpp
@@ -2,6 +2,7 @@
 #include <boost/bind.hpp>
 #include <boost/asio.hpp>
 #include <cstdlib>
+#include <cstdint>
 
 using boost::asio::ip::udp;
 using namespace std;
@@ -12,14 +13,14 @@ const int MAX_LENGTH = 1024;
 int
 main()
 {
-    boost::asio::io_service io_service;
-    udp::socket socket(io_service, udp::endpoint(udp::v4(), PORT));
+	boost::asio::io_service io_service;
+	udp::socket socket(io_service, udp::endpoint(udp::v4(), PORT));
 
-    while(1) {
-        char data[MAX_LENGTH];
-        udp::endpoint remote_endpoint;
-        size_t recv_length = socket.receive_from(boost::asio::buffer(data, MAX_LENGTH), remote_endpoint);
-        cerr << remote_endpoint.address() << ": " << data << "\n";
-        socket.send_to(boost::asio::buffer("hotdog\n", 7), remote_endpoint);
-    }
+	while(1) {
+		char data[MAX_LENGTH];
+		udp::endpoint remote_endpoint;
+		size_t recv_length = socket.receive_from(boost::asio::buffer(data, MAX_LENGTH), remote_endpoint);
+		cerr << remote_endpoint.address() << ": " << data << "\n";
+		socket.send_to(boost::asio::buffer("hotdog\n", 7), remote_endpoint);
+	}
 }
