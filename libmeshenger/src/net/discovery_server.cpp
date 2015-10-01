99181332bc4eb5b4dad64021703daf2bbf07ecf2
Keane O'Kelley <korockinout13@gmail.com>
2015-10-01 11:06:18 -0500

    Update and comment discovery_server

--- libmeshenger/src/net/discovery_server.cpp
+++ libmeshenger/src/net/discovery_server.cpp
@@ -10,26 +10,47 @@ using namespace std;
 
 int32_t MAX_LENGTH = 1024;
 
-class Listener
+namespace libmeshenger
 {
-    public:
-        Listener(boost::asio::io_service& io_service, u_int16_t  port_)
-            : io_service(io_service),
-			listen_socket(io_service, udp::endpoint(udp::v4(), port))
-		{
-			listen_socket.async_receive_from(
-				boost::asio::buffer(data, MAX_LENGTH), remote_endpoint,
-				boost::bind(&Listener::accept_conn, this, 0, 0));
-		}
+    /* Class for the UDP discovery server */
+    class DiscoveryListener final
+    {
+        private:
+            /* IO service for async asio operations */
+            boost::asio::io_service& io_service;
+            /* Socket the server will listen on*/
+            udp::socket listen_socket;
+            /* Endpoint for any remote connections */
+            udp::endpoint remote_endpoint;
+            /* UDP port number to listen on */
+            u_int16_t port;
+            /* Temporary/unused: data received on the socket */
+            string data;
+        public:
+            /* Default constructor */
+            DiscoveryListener(boost::asio::io_service& io_service, u_int16_t  port_)
+                /* Initialize io_service and socket */
+                : io_service(io_service),
+                listen_socket(io_service, udp::endpoint(udp::v4(), port))
+            {
+                /* Handle any incoming connections asynchronously */
+                listen_socket.async_receive_from(
+                    boost::asio::buffer(data, MAX_LENGTH), remote_endpoint,
+                    /* Bind connection to accept_conn method */
+                    boost::bind(&DiscoveryListener::accept_conn, this, 0, 0));
+            }
 
-		void accept_conn(const boost::system::error_code& error, size_t len)
-		{
+            /* Constructs node object from remote endpoint and sends a reply */
+            void accept_conn(const boost::system::error_code& error, size_t len)
+            {
+                /* TODO: Construct node object if node is previously unseen */
 
-		}
-	private:
-		boost::asio::io_service& io_service;
-		udp::socket listen_socket;
-		udp::endpoint remote_endpoint;
-		u_int16_t port;
-		string data;
-};
+            }
+
+            /* Sends a discovery reply to a remote node */
+            void send_discovery_reply()
+            {
+
+            }
+    };
+}
