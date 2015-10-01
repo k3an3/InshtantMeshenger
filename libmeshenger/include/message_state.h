c78b4992d6c2cf4937572f4af79bcf2d636029a6
Jake Drahos <drahos@iastate.edu>
2015-10-01 10:55:07 -0500

    Added callback stuff to header file for state

--- libmeshenger/include/message_state.h
+++ libmeshenger/include/message_state.h
@@ -22,6 +22,18 @@ namespace libmeshenger
 			 * retransmitted) or false if it has already been "seen"
 			 * */
 			bool IsMessageNew(Message m);
+
+			/* Add a callback function pointer in the form
+			 * of 'void MyCallback(Message m)' to the engine. During processing,
+			 * the message will be sent to each callback if
+			 * it is new.
+			 * */
+			void AddCallback(/*TYPE OF FUNCTION POINTER*/ fp); 
+
+			/* Process a message, sending it to all callbacks if
+			 * it is "new"
+			 * */
+			void ProcessMessage(Message m);
 	}
 }
 
