4b46e8d4d9eeb1330ca2bd28afc294d7d00013e7
Jake Drahos <drahos@iastate.edu>
2015-10-14 15:30:43 -0500

    Adjusted TestNode
    It will work once Net is implemented

--- misc/test-node/main.cpp
+++ misc/test-node/main.cpp
@@ -11,8 +11,17 @@ using namespace libmeshenger;
 
 void PrintMessage(Message m)
 {
-	/* Unimplemented BodyString method */
+	/* Print the message (Fully implemented) */
 	cout << m.BodyString() << endl;
+}
+
+/* Sort of a closure, needed for callback magic */
+static Net net;
+void ForwardMessageToPeers(Message m)
+{
+	/* Encapsulate message in packet */
+	Packet p(m);
+	net.SendToAllPeers(p);
 }
 
 int main(int argc, char * argv)
@@ -22,33 +31,48 @@ int main(int argc, char * argv)
 		return 1;
 	}
 
-	/* Instantiate a net */
-	Net net(constructorArgs);
+	/* Instantiate a net. The net is static because it
+	 * is needed by the "closure" ForwardMessage */
+	net = Net(constructorArgs);
 
 	/* add a few peers */
-	net.AddPeer();
-	net.AddPeer();
+	net.AddPeer("10.0.0.0");
+	net.AddPeer("128.8.8.8");
 
-	/* Instantiate a MessageState */
-	MessageState engine(args);
+	/* Instantiate a packet engine
+	 *
+	 * This is 100% functional */
+	PacketEngine engine();
 
-	/* Register the two callbacks */
-	MessageState.registerCallback(PrintMessage);
-	MessageState.registerCallback(net.SendToAllPeers);
+	/* Register the two callbacks 
+	 *
+	 * This is currently 100% functional*/
+	MessageState.AddCallback(PrintMessage);
+	MessageState.AddCallback(ForwardMessage);
 
+	/* Start listening asynchronously */
 	net.StartListen();
 	while (true) {
 		/* Main loop */
 
-		/* Check for any inbound connections */
+		/* Check for any inbound connections 
+		 * This will accept() any connections and
+		 * recv() from them (not sure what boost does to
+		 * abstract those two functions). Once the connection
+		 * hangs up, the net will parse it through ValidatePacket,
+		 * check the packet type (should be 0x01), then send it to
+		 * the Packet constructor and store it somewhere. GetPacket()
+		 * returns a stored packet */
 		Message m;
-		if (net.CheckIncoming())
-			m = net.GetMessage();
+		if (net.CheckAndReceive())
+			p = net.GetPacket();
 
 		/* Give message to the engine. If it's a new message, it will
 		 * be passed to the callbacks (Print and SendToAllPeers), otherwise
-		 * nothing will be done with it */
-		engine.ProcessMessage(m);
+		 * nothing will be done with it.
+		 *
+		 * This is currently 100% functional */
+		engine.ProcessPacket(p);
 	}
 
 }
