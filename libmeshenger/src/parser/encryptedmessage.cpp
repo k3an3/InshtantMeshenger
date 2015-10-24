25b3fd2ac7039a011cd4de2fe42b097e3f252cfb
Jake Drahos <drahos@iastate.edu>
2015-10-24 17:13:42 -0500

    Added EncryptedMessage

---
+++ libmeshenger/src/parser/encryptedmessage.cpp
@@ -0,0 +1,62 @@
+/* encryptedmessage.cpp
+ */
+#include <vector>
+#include <string>
+#include <cstdint>
+
+#include <parser.h>
+
+using namespace std;
+
+namespace libmeshenger
+{
+
+	EncryptedMessage::EncryptedMessage(string s)
+	{
+		m_body_dec = vector<uint8_t>(s.c_str(), s.c_str()+s.size());
+		m_decrypted = true;
+		m_encrypted = false;
+	}
+
+	EncryptedMessage::EncryptedMessage(Packet &p)
+	{
+		if (p.type() != 0x02)
+			throw WrongPacketTypeException();
+
+		m_body_enc = vector<uint8_t>(p.body());
+		m_decrypted = false;
+		m_encrypted = true;
+	}
+
+	bool
+	EncryptedMessage::encrypted()
+	{
+		return m_encrypted;
+	}
+
+	bool
+	EncryptedMessage::decrypted()
+	{
+		return m_decrypted;
+	}
+
+	vector<uint8_t>
+	EncryptedMessage::encryptedBody()
+	{
+		if (!m_encrypted) {
+			throw PacketStateException("Packet not encrypted!");
+		}
+	}
+
+	vector<uint8_t>
+	EncryptedMessage::decryptedBody()
+	{
+		if (!m_decrypted) {
+			throw PacketStateException("Packet not decrypted!");
+		}
+	}
+
+	PacketStateException::PacketStateException(string const& error)
+		: runtime_error(error)
+	{}
+}
