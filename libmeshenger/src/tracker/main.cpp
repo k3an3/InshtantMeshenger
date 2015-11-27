7b88726f18f42e3383babdf2a7320384d3f60253
Keane O'Kelley <korockinout13@gmail.com>
2015-11-27 00:53:20 -0600

    Initial stubs for MeshTrack HTTP client

---
+++ libmeshenger/src/tracker/main.cpp
@@ -0,0 +1,8 @@
+#include <tracker.h>
+#include <parser.h>
+
+int
+main()
+{
+
+}
---
+++ libmeshenger/src/tracker/tracker.cpp
@@ -0,0 +1,14 @@
+#include <boost/network/protocol/http/client.hpp>
+#include <string>
+#include <iostream>
+
+#include <parser.h>
+
+using namespace std;
+using namespace boost::network;
+using namespace boost::network::http;
+
+namespace libmeshenger
+{
+
+}
